86b7a4c770f785b91f579946a7896c8f16eb909c
FO4A5OY <jakobis95@gmail.com>
2021-12-16 18:38:14 +0100

    Suchfunktion für Excel XLSX datein umgesetzt

--- NavigateInExcel.py
+++ NavigateInExcel.py
@@ -1,13 +1,59 @@
 from openpyxl import Workbook
 import openpyxl
+from datetime import datetime
 
-file = "NavigationSheet.xlsx"
-#wb = openpyxl.Workbook(file)
-wb = openpyxl.load_workbook(filename='NavigationSheet.xlsx')
-ws = wb.active
-i=0
-for row in ws.iter_rows(min_row=1, max_col=3, max_row=2):
-    for cell in row:
-        print(cell)
+def searchXL(ws, searchTerm, searchArea = 0, rowcol = "all"):
+    Ycor = "notFound"
+    Xcor = "notFound"
 
-wb.save('NavigationSheet.xlsx')
+    if searchArea != 0 or rowcol != "all":
+        if rowcol == "row":
+            minRow = searchArea
+            maxRow = minRow
+            minCol = 0
+            maxCol = ws.max_column
+        elif rowcol =="col":
+            minCol = searchArea
+            maxCol = minCol
+            minRow = 0
+            maxRow = ws.max_row
+    else:
+        minCol = 0
+        minRow = 0
+        maxCol = ws.max_column
+        maxRow = ws.max_row
+
+
+    print("Fehlerhaft am " + datetime.today().strftime('%d.%m.'))
+    # for row in ws.iter_rows(max_col=1, max_row=10):
+    #     for cell in row:
+    #         print(cell.value)
+    #         if cell.value == "ID":
+    #             print(cell.column)  # change column number for any cell value you want
+    #             print("break")
+    #             break
+
+    for col in ws.iter_cols(min_row=minRow, max_row=maxRow, min_col=minCol, max_col= maxCol):
+        for cell in col:
+            if cell.value == searchTerm:
+                Ycor = cell.row
+                Xcor = cell.column
+                print("Suchbegriff an Coordinate[" + str(cell.row) + "," + str(cell.column) + "] gefunden" )  # change column number for any cell value you want
+                break
+
+    if Xcor == "notFound":
+        print("nichts gefunden")
+
+    return Ycor, Xcor
+
+
+
+if __name__ == "__main__":
+    file = "NavigationSheet.xlsx"
+    # wb = openpyxl.Workbook(file)
+    wb = openpyxl.load_workbook(filename=file)
+    ws = wb.worksheets[0]
+    #searchTerm = "Fehlerhaft am " + datetime.today().strftime('%d.%m.%Y')
+    searchTerm = "ID"
+    Coordinates = searchXL(ws,searchTerm)
+    wb.save('NavigationSheet.xlsx')
