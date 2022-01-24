fd87f4cb34fc3648e096b0544be2206dd77c9763
FO4A5OY <jakobis95@gmail.com>
2022-01-24 12:30:15 +0100

    Todos hinzugefügt

--- FillCbxStatus.py
+++ FillCbxStatus.py
@@ -1,4 +1,5 @@
 import json
+import os
 from Runner import refreshT
 from openpyxl import Workbook,load_workbook
 import requests
@@ -24,7 +25,7 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
     if TodayColumn != "notFound":
         print("XX today Column is:", TodayColumn)
         #Fill in all destinations that are currently offline
-        for item in offlineCBX['content']:
+        for item in offlineCBX:
             if item['uniqueId'][17] == "1":
                 cp = 0
                 searchTerm = item['uniqueId']
@@ -38,16 +39,17 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
             if foundRow != "notFound":
                 StatusWB.cell(row=foundRow, column=TodayColumn).value = "offline"
                 StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
-                TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'Gefunden'
             else:
-                TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'nicht Gefunden'
-            TodayFehlerWB.cell(row=todayCbxCounter, column=3).value = 'Offline'
+
+            TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
+            TodayFehlerWB.cell(row=todayCbxCounter, column=3).value = item['masterData']['chargePointName']
+            TodayFehlerWB.cell(row=todayCbxCounter, column=4).value = 'Fehler'
             todayCbxCounter = todayCbxCounter + 1
 
         # Fill in all destinations that currently have a failure
-        for item in fehlerCBX['content']:
+        for item in fehlerCBX:
             if item['uniqueId'][17] == "1":
                 cp = 0
                 searchTerm = item['uniqueId']
@@ -58,17 +60,20 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
 
             foundRow = searchXL(StatusWB, searchTerm, 1, "col")[0]
             print("XX ", foundRow)
+
             if foundRow != "notFound":
                 StatusWB.cell(row=foundRow, column=TodayColumn).value = "j"
                 StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
-                TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'Gefunden'
             else:
-                TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'nicht Gefunden'
-            TodayFehlerWB.cell(row=todayCbxCounter, column=3).value = 'Fehler'
-            todayCbxCounter = todayCbxCounter + 1
 
+            TodayFehlerWB.cell(row=todayCbxCounter, column=1).value = item['uniqueId']
+            TodayFehlerWB.cell(row=todayCbxCounter, column=3).value =  item['masterData']['chargePointName']
+            TodayFehlerWB.cell(row=todayCbxCounter, column=4).value = 'Fehler'
+            todayCbxCounter = todayCbxCounter + 1
+    # TODO: Gelb maikieren wenn Zustandänderung
+    # TODO: Alle nicht befüllten Felder mit "n" makieren
     else:
         print("Es konnte kein Spalte mit dem heutigen Datum gefunden werden. Prüfen Sie die Excel-datei.")
     # print(searchXL(StatusWB, "Hello was geht"))
@@ -90,11 +95,15 @@ if __name__ == "__main__":
     f = open("fehlerstandorteStatus.text", 'r')
     fehlerCBX = json.load(f)
     for element in fehlerCBX:
-        print(element)
+        for item in element:
+            print(item)
 
     f = open("offlinestandorte.text", 'r')
     offlineCBX = json.load(f)
     for element in offlineCBX:
         print(element)
 
-    WriteStatusToXL(xlsxPfad, offlineCBX, offlineCBX)
+    #WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX)
+    xlsxPfad = r"C:\Users\FO4A5OY\OneDrive - Dr. Ing. h.c. F. Porsche AG\LE_Failure_Analysis\CBX_Fehlerliste_AutoPyTesting.xlsx"
+    #os.system('start  "excel" "C:\\Users\\FO4A5OY\\OneDrive - Dr. Ing. h.c. F. Porsche AG\\LE_Failure_Analysis\\CBX_Fehlerliste_AutoPyTesting.xlsx"')
+    os.startfile(xlsxPfad)
