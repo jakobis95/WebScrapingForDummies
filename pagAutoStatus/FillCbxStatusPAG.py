3e4a60dcbb43f66c7b49b9c66f63562d43b59529
AJ2MSGR <jakobis95@gmail.com>
2022-05-10 15:17:50 +0200

    Status in neue tabelle schreibbar

--- pagAutoStatus/FillCbxStatusPAG.py
+++ pagAutoStatus/FillCbxStatusPAG.py
@@ -11,23 +11,25 @@ def changes(xlsxPfad, offlineCBX, fehlerCBX):
 def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
     cp = 0
     wb = load_workbook(filename=xlsxPfad)
-    wbfeedback = load_workbook(filename=xlsxPfadFeedback)
     todayCbxCounter = 1
-    StatusWB = wb["STATUS"]
+    StatusWB = wb["StatusKurz"]
     TodayFehlerWB = wb["overviewToday"]
     NIBfehlerWB = wb["NIBfehler"] #NIB steht für "nicht im backend"
     NIBofflineWB = wb["NIBoffline"]
     LastRow = StatusWB.max_row
-    CW = datetime.today()
-    TodayColumnString = "Full/Part KW" + datetime.today()[1]-1
+    CW = datetime.today().isocalendar()
+    TodayColumnString = "Full/Part KW" + str(CW[1]) #todo gibt diese den Namen der Woche an.
     #finds Column with the date of today
-    TodayCell = searchXL(StatusWB, TodayColumnString) #todo ist eigentlich die Spalte der letzten Abfrage
+    CPMID = searchXL(StatusWB, "CPM ID")  # findet jetzt die heutige Spalte
+    cpmidColumn = CPMID[1]
+    TodayCell = searchXL(StatusWB, TodayColumnString) #findet jetzt die heutige Spalte
     TodayColumn = TodayCell[1]
     TodayRow = TodayCell[0]
     #finds Column to display if cp1 and/or cp2 has a problem
-    cp1Column = searchXL(StatusWB, "1")[1]
-    cp2Column = searchXL(StatusWB, "2")[1]
-    cpColumn = [cp1Column,cp2Column]
+    #todo wird nicht gebraucht
+    # cp1Column = searchXL(StatusWB, "1")[1]
+    # cp2Column = searchXL(StatusWB, "2")[1]
+    # cpColumn = [cp1Column,cp2Column]
 
     if TodayColumn != "notFound":
         print("XX today Column is:", TodayColumn)
@@ -41,11 +43,11 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
                 searchTerm = item['uniqueId']
                 searchTerm = searchTerm[:17] + "1"
 
-            foundRow = searchXL(StatusWB, searchTerm, 1, "col")[0]
+            foundRow = searchXL(StatusWB, searchTerm, cpmidColumn, "col")[0]
             print("XX ", foundRow)
             if foundRow != "notFound":
                 StatusWB.cell(row=foundRow, column=TodayColumn).value = "offline"
-                StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
+                #StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'Gefunden'
             else:
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'nicht Gefunden'
@@ -67,15 +69,16 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
                 searchTerm = searchTerm[:17] + "1"
 
             print(searchTerm)
-            foundRow = searchXL(StatusWB, searchTerm, 1, "col")[0]
+            foundRow = searchXL(StatusWB, searchTerm, cpmidColumn, "col")[0]
             print("XX ", foundRow)
             print(item['ErrorMessage'])
 
             if foundRow != "notFound":
                 StatusWB.cell(row=foundRow, column=TodayColumn).value = "j"
-                StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
+                #StatusWB.cell(row=foundRow, column=cpColumn[cp]).value = "x"
                 if StatusWB.cell(row=foundRow, column=TodayColumn-1).value != "j":
-                    StatusWB.cell(row=foundRow, column=9).value = item['ErrorMessage']
+                    print(item['ErrorMessage'])
+                    #StatusWB.cell(row=foundRow, column=9).value = item['ErrorMessage']
                 TodayFehlerWB.cell(row=todayCbxCounter, column=2).value = 'Gefunden'
                 TodayFehlerWB.cell(row=todayCbxCounter, column=5).value = item['ErrorMessage']
             else:
@@ -120,29 +123,29 @@ def WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX):
 
 
 if __name__ == "__main__":
-    # UserName = os.getlogin()
-    # xlsxPfad = "C:\\Users\\" + str(UserName) + "\\Desktop\\22_04_21_Tracking IBN_KW18.xlsx"
-    # xlsxPfadFeedback = "C:\\Users\\" + str(UserName) + "\\Desktop\\TrackingFeedback.xlsx"
-    #
-    # f = open("A2WorkingSkrips/DataFiles/fehlerstandorteStatus.text", 'r')
-    # fehlerCBX = json.load(f)
-    # for element in fehlerCBX:
-    #     for item in element:
-    #         print(item)
-    #
-    # f = open("A2WorkingSkrips/DataFiles/offlinestandorte.text", 'r')
-    # offlineCBX = json.load(f)
-    # for element in offlineCBX:
-    #     print(element)
-    #
-    # WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX)
     UserName = os.getlogin()
-    xlsxPfad = "C:\\Users\\" + str(UserName) + "\\Downloads\\220506_Tracking IBN_KW18_Masterliste_Kopie.xlsx"
+    xlsxPfad = "C:\\Users\\" + str(UserName) + "\\Downloads\\220506_Tracking IBN_KW18_Masterliste.xlsx"
+    xlsxPfadFeedback = "C:\\Users\\" + str(UserName) + "\\Desktop\\TrackingFeedback.xlsx"
 
-    wb = load_workbook(filename=xlsxPfad)
-    StatusWB = wb["STATUS"]
-    #StatusWB.insert_cols(1)
+    f = open("C:/Users/AJ2MSGR/PycharmProjects/WebScrapingForDummies/A2WorkingSkrips/DataFiles/fehlerstandorteStatus.text", 'r')
+    fehlerCBX = json.load(f)
+    for element in fehlerCBX:
+        for item in element:
+            print(item)
+
+    f = open("C:/Users/AJ2MSGR/PycharmProjects/WebScrapingForDummies/A2WorkingSkrips/DataFiles/offlinestandorte.text", 'r')
+    offlineCBX = json.load(f)
+    for element in offlineCBX:
+        print(element)
+
+    WriteStatusToXL(xlsxPfad, offlineCBX, fehlerCBX)
+    #UserName = os.getlogin()
+    #xlsxPfad = "C:\\Users\\" + str(UserName) + "\\Downloads\\220506_Tracking IBN_KW18_Masterliste_Kopie.xlsx"
+
+    #wb = load_workbook(filename=xlsxPfad)
+    #StatusWB = wb["StatusKurz"]
+    #StatusWB.insert_cols(6, 3) #bevor row 6 insert 3 rows
     #StatusWB.insert_cols("A")
-    StatusWB.cell(row=1, column=1).fill = "Ist das ne neue Spalte"
-    wb.save(xlsxPfad)
+    #StatusWB.cell(row=1, column=1).value = "Ist das Durch gegange"
+    #wb.save(xlsxPfad)
     os.startfile(xlsxPfad)
