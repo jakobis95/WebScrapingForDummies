dcbf7cd8846657cbf4fac20a8d74a929aa1b87d0
AJ2MSGR <jakobis95@gmail.com>
2022-05-31 10:23:25 +0200

    Machst garnichts mehr

--- JiraAuto/GetAllPDF.py
+++ JiraAuto/GetAllPDF.py
@@ -1,6 +1,7 @@
 import glob, os
 from PDFreader import readServiceProt
-
+i = 1
+keyList = ["KLL_dealerID", "KLL_location","Ort Datum", "KLL_causeError", "KLL_customerComplaint", "KLL_detailedError"]
 path_dir = "X:/Proj/V/V0019/E_Mobility_Charging_Solutions/06_IT Systeme/05 Non-Automotive Stammdatenbank/04_DC_Ladehardware/04_Serviceprotokolle"
 for root, dirs, files in os.walk(path_dir):
     for dir in dirs:
@@ -13,4 +14,4 @@ for root, dirs, files in os.walk(path_dir):
                     #print(file)
                     path_dir_dir_pdf = str(path_dir_dir) + "/" + str(file)
                     print(path_dir_dir_pdf)
-                    readServiceProt(path_dir_dir_pdf)
+                    #print(readServiceProt(path_dir_dir_pdf, keyList))
