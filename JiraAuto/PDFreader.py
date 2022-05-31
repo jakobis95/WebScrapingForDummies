f416e6c14c494957c07b62096025834e04b6a8fb
AJ2MSGR <jakobis95@gmail.com>
2022-05-31 10:23:11 +0200

    Gibt ein Liste mit dem Inhalt der PDF zurück

--- JiraAuto/PDFreader.py
+++ JiraAuto/PDFreader.py
@@ -4,8 +4,11 @@ import pprint
 import os
 from os.path import exists
 
-def readServiceProt(Path):
-
+def readServiceProt(Path, keyList):
+    error = False
+    fileName = Path.split("/")[-1]
+    dirName = Path.split("/")[-2]
+    feedBack = [dirName, fileName, "no entry", "empty", "empty", "empty", "empty", "empty", "empty"]
     # creating a pdf file object
     if exists(Path):
         pdfFileObj = open(Path, 'rb')
@@ -26,38 +29,39 @@ def readServiceProt(Path):
             # create a field object
             fieldObj = pdfReader.getFields()
 
-            # try:
-            #     fieldObj
-            # except NameError:
-            #     fieldObj = {"error" : "error" }
-
             if fieldObj != None:
                 if len(fieldObj) > 0:
-                    print(fieldObj["KLL_dealerID"])
-                    print(fieldObj["KLL_location"])
-                    print(fieldObj["KLL_detailedError"])
+                    i = 3
+                    feedBack[2] = "found"
+                    for key in keyList:
+                        try:
+                            vValue = fieldObj[key]["/V"]
+                        except:
+                            vValue = "Leer"
+                        feedBack[i] = vValue
+                        i = i + 1
                 else:
                     print("fehlerhaft")
+                    error = True
             else:
                 print("fehlerhaft")
-            #format dict nicely
+                error = True
 
-            # Prints the nicely formatted dictionary
-            #pprint.pprint(fieldObj)
-
-            # Sets 'pretty_dict_str' to the formatted string value
-            pretty_dict_str = pprint.pformat(fieldObj)
-
-            #for field in fieldObj:
-            #print(fieldObj["KLL_dealerID"])
-            # closing the pdf file object
+        # closing the pdf file object
         pdfFileObj.close()
+        if error:
+            feedBack[2] = "unfindable"
+        return feedBack
     else:
         print("Datei Existiert nicht")
-        return "error"
+        feedBack[2] = "unfindable"
+        return feedBack
 
 if __name__ == "__main__":
     UserName = os.getlogin()
     path = "X:/Proj/V/V0019/E_Mobility_Charging_Solutions/06_IT Systeme/05 Non-Automotive Stammdatenbank/04_DC_Ladehardware/04_Serviceprotokolle/132_Bergamo_20210928/07_04_Annex_I_Service_Report_KLL_Template_en 01-09-2021.pdf"
-    #path = "C:\\Users\\" + str(UserName) + "\\Downloads\\FehlerhaftePDF.pdf"
-    readServiceProt(path)
+    #path = "C:\\Users\\" + str(UserName) + "\\Downloads\\TEST_PDF_Lesen.pdf"
+    keyList = ["KLL_dealerID", "KLL_location","Ort Datum", "KLL_causeError", "KLL_customerComplaint", "KLL_detailedError"]
+    failedReads = []
+    successfulReads = []
+    print(readServiceProt(path, keyList))
