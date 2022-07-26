fe2e66669d13dce3170ba70897618996f53505c6
AJ2MSGR <jakobis95@gmail.com>
2022-07-26 15:51:13 +0200

    Alle Funktionien des Reportings werden in einem ausgeführt. Filepropertys können gelesen werden um im nächsten Schritt die Aktualität der Dokumente zu Prüfen.

--- UserInterface/Main.py
+++ UserInterface/Main.py
@@ -2,41 +2,71 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 import sqlite3
-class GUI():
-    def __init__(self, Frame, *args, **kwargs):
-        self.work_window = Toplevel()
-        self.q = (StringVar(), 'Schwierigkeit'), (StringVar(), 'Typ'), (StringVar(), 'Titel'), (StringVar(), 'Author'), (StringVar(), 'Datum'), (StringVar(), 'Author2'), (StringVar(), 'Datum2')
-        self.titel = StringVar()
-        WIDTH = int(Frame.winfo_screenwidth() / 2)
-        HEIGHT = int(Frame.winfo_screenheight() / 2)
-        self.work_window.title("DB_List")
-        self.work_window.resizable(False, False)
-        self.work_window.geometry("%dx%d" % (WIDTH, HEIGHT))
-        #self.UI_Elemente()
+import json
+import os
+import requests
+from pagAutoStatus.FillCbxStatusPAG import WriteStatusToXL
+from pagAutoStatus.Jira_Bugs_to_XL import write_Bugs_to_XL
+from pagAutoStatus.Update_VR16_HVAC_xl import start_Update_from_Jira
+from A2WorkingSkrips.CBXStatusUpdate import BackendRequestTemplate,cpstate,authLoopRequest,get_error_msg
 
-        self.Read_Entry_btn = Button(self.work_window, text="read entry", command=self.printSelf)
-        self.Read_Entry_btn.pack(side=tk.TOP, padx=6, anchor="s")
+def get_File_property():
+    from pathlib import *
+    p = Path("C:\\Users\\" + str(UserName) + "\\Downloads\\HVACOverview.xlsx")
+    pct = p.stat().st_mtime
+    from datetime import datetime
+    print(datetime.utcfromtimestamp(pct).strftime('%Y-%m-%d %H:%M:%S'))
 
-        self.Add_Entry_btn = Button(self.work_window, text="Frage in DB erstellen", command=self.printSelf)
-        self.Add_Entry_btn.pack(side=tk.TOP, padx=6, anchor="s")
-
-        self.Save_btn = Button(self.work_window, text="Save Changes", command=self.printSelf)
-        self.Save_btn.pack(side=tk.TOP, padx=6, anchor="s")
-
-        self.Search_Entry = Entry(self.work_window, textvariable=self.titel)
-        self.Search_Entry.pack(side=tk.TOP, padx=6, anchor="s")
-
-    def printSelf(self):
-        print("Self")
 if __name__ == "__main__":
-    root = tk.Tk()
-    WIDTH = int(root.winfo_screenwidth() / 1.5)
-    HEIGHT = int(root.winfo_screenheight() / 2)
-    root.title("DB_List")
-    root.resizable(False, False)
-    root.geometry("%dx%d" % (WIDTH, HEIGHT))
-    gesucht = 'Spannungsteiler 2'
-    dbname = '../testdb.db'
-    lbl = tk.Label(text="Das ist das Main Window")
-    Fragen_Frame = GUI(root, gesucht)
-    root.mainloop()
+
+#Data directory
+    UserName = os.getlogin()
+    jira_xlsx_path_HVAC = "C:\\Users\\" + str(UserName) + "\\Downloads\\HVACOverview.xlsx"
+    jira_xlsx_path_VR16 = "C:\\Users\\" + str(UserName) + "\\Downloads\\VR16UpdatedStations.xlsx"
+    master_xlsx_path = "C:\\Users\\" + str(UserName) + "\\Downloads\\IBN_Tracking.xlsx"
+    jira_CSV_Path = "C:\\Users\\" + str(UserName) + "\\Downloads\\current_bugs.xlsx"
+
+#CBXStatusUpdate
+    i = 0
+    urllist = ["https://api.chargepoint-management.com/chargepoint/chargepoints/list?page=0&size=500&sort=masterData.chargePointName,asc&masterData.chargingFacilities.powerType=DC&status=FAULTED",
+            "https://api.chargepoint-management.com/chargepoint/chargepoints/list?page=0&size=500&sort=masterData.chargePointName,asc&masterData.chargingFacilities.powerType=DC&status=INACTIVE"
+            ]
+    s = requests.session()
+    authLoopRequest(s)
+    with open('DataFiles/refreshtoken.txt', 'r') as jsonf:
+        data = json.load(jsonf)
+        print( data['refresh_token'])
+    atoken = 'Bearer ' + data['access_token']
+    i = 0
+    fehlerstandorte = BackendRequestTemplate(atoken,urllist[0],s,i) #typ = fehler
+    i = 1
+    get_error_msg(fehlerstandorte)
+    offlinestandorte = BackendRequestTemplate(atoken, urllist[1], s, i ) #typ = offline
+    f = open("DataFiles/offlinestandorte.text", 'w')
+    f.write(json.dumps(offlinestandorte))
+    authLoopRequest(s)
+    with open('DataFiles/refreshtoken.txt', 'r') as jsonf:
+        data = json.load(jsonf)
+        print(data['refresh_token'])
+    atoken = 'Bearer ' + data['access_token']
+    fehlerstandorteStatus = cpstate(fehlerstandorte)
+    f = open("DataFiles/fehlerstandorteStatus.text", 'w')
+    f.write(json.dumps(fehlerstandorteStatus))
+
+#FillCbxStatusPAG
+
+    f = open("C:/Users/AJ2MSGR/PycharmProjects/WebScrapingForDummies/A2WorkingSkrips/DataFiles/fehlerstandorteStatus.text", 'r')
+    fehlerCBX = json.load(f)
+    f = open("C:/Users/AJ2MSGR/PycharmProjects/WebScrapingForDummies/A2WorkingSkrips/DataFiles/offlinestandorte.text", 'r')
+    offlineCBX = json.load(f)
+    WriteStatusToXL(master_xlsx_path, offlineCBX, fehlerCBX)
+
+#Update_VR16_HVAC_xl
+    start_Update_from_Jira(jira_xlsx_path_HVAC,jira_xlsx_path_VR16,master_xlsx_path)
+
+#Jira_Bugs_to_XL
+    write_Bugs_to_XL(jira_CSV_Path, master_xlsx_path)
+
+#Finished Prozess open complete file
+    os.startfile(master_xlsx_path)
+
